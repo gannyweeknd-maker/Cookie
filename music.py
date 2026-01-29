7392c2038670c061b3df23e8827f2973d4ad0579
gannyweeknd-maker <ganny.weeknd@gmail.com>
2026-01-29 12:00:16 +0530

    Create music.py

---
+++ music.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def play(self, ctx, url):
+        if not ctx.author.voice:
+            return await ctx.send("Join a VC first")
+
+        vc = ctx.voice_client
+        if not vc:
+            vc = await ctx.author.voice.channel.connect()
+
+        vc.play(discord.FFmpegPCMAudio(url))
+        await ctx.send("🎵 Playing")
+
+    @commands.command()
+    async def stop(self, ctx):
+        if ctx.voice_client:
+            await ctx.voice_client.disconnect()
+
+async def setup(bot):
+    await bot.add_cog(Music(bot))
